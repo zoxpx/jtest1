6333cd98cb4fde63a9f18a30344866e08eeabf6c
Zoran Rajic <zox@portworx.com>
2018-05-31 21:12:45 -0700

    Adding buggy func3

--- test1.go
+++ test1.go
@@ -5,7 +5,12 @@ func JenkinsTest1() string {
 	return "Hello from Jenkins test #1"
 }
 
-// JenkinsTest1 is just a dummy test
+// JenkinsTest2 is just a dummy test
 func JenkinsTest2() string {
 	return "Hello from Jenkins test #2"
+}
+
+// JenkinsTest2 is just a dummy test
+func JenkinsTest3() string {
+	return "Jenkins test #3 says 'Go away'"
 }
